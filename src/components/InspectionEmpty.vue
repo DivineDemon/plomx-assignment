532db6af68487b31b75849866b6fcaacc836b717
DivineDemon <supame123@gmail.com>
2022-10-27 20:09:03 +0500

    InspectionEmpty.vue! :sparkles:

---
+++ src/components/InspectionEmpty.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-xl font-bold ml-5 mb-3">New quality check</h1>
+    <div
+      class="bg-white border-4 border-gray-200 w-[500px] h-64 flex flex-col space-y-4 justify-center items-center align-center"
+    >
+      <div class="text-center">
+        <h1 class="font-bold">Inspection checks</h1>
+        <p class="font-sem-bold text-gray-500">
+          There are no checks selected yet.
+        </p>
+      </div>
+      <button
+        class="text-white font-semibold text-xs bg-[#0b84fe] py-2 px-3 rounded-sm"
+      >
+        Add first check
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InspectionEmpty",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,25 @@
   <div class="home">
     <NavBar />
     <SubMenu />
+    <div class="mt-5 grid place-items-center">
+      <!-- Inspection Check -->
+      <InspectionEmpty />
+      <!-- Inspection -->
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/NavBar.vue";
 import SubMenu from "@/components/common/SubMenu.vue";
+import InspectionEmpty from "@/components/InspectionEmpty.vue";
 
 export default {
   name: "HomeView",
   components: {
     NavBar,
     SubMenu,
+    InspectionEmpty,
   },
 };
 </script>
